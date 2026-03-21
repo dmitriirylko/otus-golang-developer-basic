5c5e8908e727d79ae7275f805f4f9d435bc19397
Dmitrii Rylko <dmitriirylko@gmail.com>
2026-03-22 01:58:41 +0300

    Добавлена обработка запроса имен игроков

--- hw03-13-chess/main.go
+++ hw03-13-chess/main.go
@@ -2,24 +2,48 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 )
 
-func findFieldSize() int {
+type GameConfig struct {
+	fieldSize int
+	player1   string
+	player2   string
+}
+
+func makeConfig() GameConfig {
 	const (
 		defaultFieldSize = 8
 		minFieldSize     = 1
 		maxFieldSize     = 100
+		defaultPlayer1   = "Player1"
+		defaultPlayer2   = "Player2"
 	)
-	if len(os.Args) > 1 {
-		cliFieldSize, err := strconv.Atoi(os.Args[1])
-		if err == nil && cliFieldSize >= minFieldSize && cliFieldSize <= maxFieldSize {
-			return cliFieldSize
-		}
+	var err error
+	var fieldSize int
+	var player1, player2, fieldSizeStr string
+	fmt.Print("Insert field size: ")
+	if _, err = fmt.Scan(&fieldSizeStr); err != nil {
+		fieldSize = defaultFieldSize
 	}
-	return defaultFieldSize
+	fieldSize, err = strconv.Atoi(fieldSizeStr)
+	if err != nil || fieldSize < minFieldSize || fieldSize > maxFieldSize {
+		fieldSize = defaultFieldSize
+	}
+	fmt.Print("Insert first player name: ")
+	if _, err = fmt.Scan(&player1); err != nil {
+		player1 = defaultPlayer1
+	}
+	fmt.Print("Insert second player name: ")
+	if _, err = fmt.Scan(&player2); err != nil {
+		player2 = defaultPlayer2
+	}
+	return GameConfig{
+		fieldSize: fieldSize,
+		player1:   player1,
+		player2:   player2,
+	}
 }
 
 func makeField(size int) string {
@@ -42,9 +66,7 @@ func makeField(size int) string {
 }
 
 func main() {
-	fmt.Println("Let's play chess")
-	fieldSize := findFieldSize()
-	fmt.Printf("Field size is %d\n", fieldSize)
-	field := makeField(fieldSize)
+	cfg := makeConfig()
+	field := makeField(cfg.fieldSize)
 	fmt.Print(field)
 }
